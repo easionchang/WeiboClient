030edabf76f96f560099a7ac5ec431490b90a083
Msn <easionchang@126.com>
2015-03-13 17:59:29 +0800

    动画处理,实现单图放大缩小

--- app/src/main/java/com/msn/support/gallery/GalleryItemFragment.java
+++ app/src/main/java/com/msn/support/gallery/GalleryItemFragment.java
@@ -19,6 +19,7 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.msn.support.utils.DisplayUtil;
+import com.msn.support.utils.ImageUtils;
 import com.msn.support.view.DonutProgress;
 import com.msn.weiboclient.R;
 import com.nostra13.universalimageloader.core.ImageLoader;
@@ -42,6 +43,11 @@ public class GalleryItemFragment extends Fragment {
     private int[] mLocation;
     private PhotoView itemImgv;
     private DonutProgress itemProgressView;
+
+    private float mScale;
+    private float mDeltaX;
+    private float mDeltaY;
+    private View mParentView;
 
     /**
      * 系统“短”动画持续时间（毫秒）。该时间用于细微的动画或者发生频次很高的动画上。
@@ -79,7 +85,21 @@ public class GalleryItemFragment extends Fragment {
         itemImgv.setOnViewTapListener(new PhotoViewAttacher.OnViewTapListener() {
             @Override
             public void onViewTap(View view, float v, float v2) {
-                getActivity().finish();
+                if(mScale != 0){
+                    itemImgv.animate().setDuration(mShortAnimationDuration)
+                            .scaleX(mScale)
+                            .scaleY(mScale)
+                            .translationX(mDeltaX)
+                            .translationY(mDeltaY).withEndAction(new Runnable() {
+                        @Override
+                        public void run() {
+                            GalleryItemFragment.this.getActivity().finish();
+                        }
+                    });
+                    ObjectAnimator alpha = ObjectAnimator.ofInt(mParentView.getBackground(), "alpha", 255, 0);
+                    alpha.start();
+                }
+                //getActivity().finish();
             }
         });
         itemProgressView = (DonutProgress)view.findViewById(R.id.item_pbar);
@@ -92,7 +112,7 @@ public class GalleryItemFragment extends Fragment {
             //Y的值是相对屏幕的坐标，而不是父View；所以要减去状态栏的高度（标题栏在该页面不显示）
             //itemImgv.setY(mLocation[1] - DisplayUtil.getStatusBarHeight());
             //view.getBackground().setAlpha(10);
-            startAnimate2(view,itemImgv,mLocation);
+            startAnimate(view,itemImgv,mLocation);
         }else{
             ImageLoader.getInstance().displayImage(mImgUrl,itemImgv, null,new ImageLoadingListener() {
                         public void onLoadingStarted(String imageUri, View view) {}
@@ -111,178 +131,45 @@ public class GalleryItemFragment extends Fragment {
                         }
                     });
         }
+        mParentView = view;
         return view;
     }
 
 
-    private void startAnimate2(final View parentView,final ImageView itemImgv,final int[] location){
+    private void startAnimate(final View parentView,final ImageView itemImgv,final int[] location) {
+        Log.e("Test", "itemImgv.heigh" +
+                "t=" + itemImgv.getHeight() + " itemImgv.width=" + itemImgv.getWidth());
+        int startWidth = location[2];
+        int startHeight = location[3];
+        int finalWidth = DisplayUtil.getScreenWidth();
+        int finalHeight = DisplayUtil.getScreenHeight() - DisplayUtil.getStatusBarHeight();
+        int[] imageDimension = ImageUtils.getBitmapDimension(mImgUrl);
+        int imageWidth = imageDimension[0];
+        int imageHeight = imageDimension[1];
+        float startScale = ImageUtils.getScale(imageWidth, imageHeight, startWidth, finalWidth, startHeight, finalHeight);
 
-                        Log.e("Test","itemImgv.heigh" +
-                                "t="+itemImgv.getHeight()+" itemImgv.width="+itemImgv.getWidth());
-                        int startWidth = location[2];
-                        int startHeight = location[3];
-                        int finalWidth = DisplayUtil.getScreenWidth();
-                        int finalHeight = DisplayUtil.getScreenHeight() - DisplayUtil.getStatusBarHeight();
-
-                        float startScale = getScale(startWidth,finalWidth,startHeight,finalHeight);
-
-                        itemImgv.setScaleX(startScale);
-                        itemImgv.setScaleY(startScale);
-                        float paddingTop= getPaddingTop(startScale,startWidth,finalWidth,startHeight,finalHeight);
-                        float PaddingLeftByStart = getPaddingLeftByStart(startWidth,startHeight);
-                        float PaddingTopByStart = getPaddingTopByStart(startWidth,startHeight);
-                        itemImgv.setX(PaddingLeftByStart+location[0]+(startScale*finalWidth-finalWidth)/2+getPaddingLeft(startScale,startWidth,finalWidth,startHeight,finalHeight));
-                        itemImgv.setY(PaddingTopByStart+location[1]- DisplayUtil.getStatusBarHeight()+(startScale*finalHeight-finalHeight)/2+paddingTop);
-                        //parentView.getBackground().setAlpha(10);
+        float imageActureHeight = ImageUtils.getBitmapHeight(startWidth, startHeight, imageWidth, imageHeight);
+        float imageActureWidth = ImageUtils.getBitmapWidth(startWidth, startHeight, imageWidth, imageHeight);
+        float scaleFinalPaddingTop = ImageUtils.getPaddingTop(imageActureHeight, finalHeight * startScale);
+        float scaleFinalPaddingLeft = ImageUtils.getPaddingLeft(imageActureWidth, finalWidth * startScale);
+        float startPaddingTop = ImageUtils.getPaddingTop(imageActureHeight, startHeight);
+        float startPaddingLeft = ImageUtils.getPaddingLeft(imageActureWidth, startWidth);
+        itemImgv.setScaleX(startScale);
+        itemImgv.setScaleY(startScale);
+        mScale = startScale;
+        Log.e("Test", "startPaddingLeft=" + startPaddingLeft + " startPaddingTop=" + startPaddingTop);
+        mDeltaX = location[0] + scaleFinalPaddingLeft - startPaddingLeft + (startScale * finalWidth - finalWidth) / 2;
+        mDeltaY = location[1] - DisplayUtil.getStatusBarHeight() + scaleFinalPaddingTop - startPaddingTop + (startScale * finalHeight - finalHeight) / 2;
+        itemImgv.setX(mDeltaX);
+        itemImgv.setY(mDeltaY);
+        //parentView.getBackground().setAlpha(10);
 
         itemImgv.animate().setDuration(mShortAnimationDuration)
                 .scaleX(1)
                 .scaleY(1)
-        .translationX(0).translationY(0);
+                .translationX(0).translationY(0);
 
-        ObjectAnimator alpha = ObjectAnimator.ofInt(parentView.getBackground(),"alpha",0,255);
+        ObjectAnimator alpha = ObjectAnimator.ofInt(parentView.getBackground(), "alpha", 0, 255);
         alpha.start();
-
-
-        //Bitmap bitmap = BitmapFactory.decodeFile(ImageloaderUtils.getImageDiscCached(mImgUrl).getAbsolutePath());
-
-        //int imgWidth = bitmap.getWidth();
-        //int imgHeight = bitmap.getHeight();
-
-
-        //放大后的图片尺寸，将图片最大放大到全屏
-
-//        itemImgv.setPivotX(0);
-//        itemImgv.setPivotY(0);
-//
-//        itemImgv.animate().setDuration(mShortAnimationDuration)
-//                .scaleX(1)
-//                .scaleY(1)
-//                .translationX(screenWidth/2-location[2]*scale/2.0f)
-//                .translationY((screenHeight/2-location[3]*scale/2.0f));
-//                //.translationX(screenWidth/2-imgWidth*scale/2.0f)
-//                //.translationY(screenHeight/2-imgHeight*scale/2.0f);
-//        Log.e("Test"," screenWidth="+screenWidth+" screenHeight="+screenHeight+" imgWidth="+imgWidth+" imgHeight="+imgHeight);
-//        Log.e("Test","scale="+scale+" widthScale="+screenWidth/(float) bitmap.getWidth()
-//                +" heightscale="+screenHeight/(float)bitmap.getHeight()
-//                +" translationX="+(screenWidth/2-imgWidth*scale/2.0f)
-//                +" translationY="+(screenHeight/2-imgHeight*scale/2.0f));
-
-        //Log.e("Test","mRect.left="+mRect.left+" mRect.top="+mRect.top);
-    }
-
-    private float getPaddingTop(float scale,int startWidth,int finalWidth,int startHeight,int finalHeight){
-        Bitmap bitmap = BitmapFactory.decodeFile(ImageloaderUtils.getImageDiscCached(mImgUrl).getAbsolutePath());
-        int imgWidth = bitmap.getWidth();
-        int imgHeight = bitmap.getHeight();
-
-        float finalImageViewHeight = finalHeight*scale;
-        float bitmapHeight = getBitmapHeight(startWidth,startHeight,imgWidth,imgHeight);
-        Log.e("Test","getPaddingTop==="+(bitmapHeight - finalImageViewHeight)/2);
-        return (bitmapHeight - finalImageViewHeight)/2;
-    }
-
-    private float getPaddingLeft(float scale,int startWidth,int finalWidth,int startHeight,int finalHeight){
-        Bitmap bitmap = BitmapFactory.decodeFile(ImageloaderUtils.getImageDiscCached(mImgUrl).getAbsolutePath());
-        int imgWidth = bitmap.getWidth();
-        int imgHeight = bitmap.getHeight();
-
-        float finalImageViewWidth = finalWidth*scale;
-        float bitmapWidth = getBitmapWidth(startWidth,startHeight,imgWidth,imgHeight);
-        Log.e("Test","getPaddingLeft==="+(bitmapWidth - finalImageViewWidth)/2);
-        return (bitmapWidth - finalImageViewWidth)/2;
-    }
-
-    private float getPaddingTopByStart(int startWidth,int startHeight){
-        Bitmap bitmap = BitmapFactory.decodeFile(ImageloaderUtils.getImageDiscCached(mImgUrl).getAbsolutePath());
-        int imgWidth = bitmap.getWidth();
-        int imgHeight = bitmap.getHeight();
-        float bitmapHeight = getBitmapHeight(startWidth,startHeight,imgWidth,imgHeight);
-        Log.e("Test","getPaddingTopByStart==="+(startHeight-bitmapHeight)/(float)2);
-        return (startHeight-bitmapHeight)/(float)2;
-    }
-
-    private float getPaddingLeftByStart(int startWidth,int startHeight){
-        Bitmap bitmap = BitmapFactory.decodeFile(ImageloaderUtils.getImageDiscCached(mImgUrl).getAbsolutePath());
-        int imgWidth = bitmap.getWidth();
-        int imgHeight = bitmap.getHeight();
-        float bitmapWidth = getBitmapWidth(startWidth,startHeight,imgWidth,imgHeight);
-        Log.e("Test","getPaddingLeftByStart==="+(startWidth-bitmapWidth)/(float)2);
-        return (startWidth-bitmapWidth)/(float)2;
-    }
-
-    private float getBitmapWidth(int startWidth,int startHeight,int imgWidth, int imgHeight){
-        float actureWidth;
-
-        if(imgWidth >= imgHeight){//宽的图片，最终宽度为ImageView宽度
-            actureWidth = startWidth;
-        }else{//高的，
-            float actureHeight = startHeight;
-            float bitmapScale = actureHeight/imgHeight;
-            actureWidth = bitmapScale*imgWidth;
-        }
-
-        return actureWidth;
-    }
-
-    private float getBitmapHeight(int startWidth,int startHeight,int imgWidth, int imgHeight){
-        float actureHeight;
-
-        if(imgWidth <= imgHeight){//高的图片，最终高度为ImageView高度
-            actureHeight = startHeight;
-        }else{//宽的，
-            float actureWidth = startWidth;
-            float bitmapScale = actureWidth/imgWidth;
-            actureHeight = bitmapScale*imgHeight;
-        }
-
-        return actureHeight;
-    }
-
-    private float getBitmapScaleInImageView(int imgWidth,int imgHeight,int startWidth,int startHeight){
-        float imgScale;//图片实际尺寸显示在startWidth，startHeight中的缩放比例
-        if(imgWidth >= imgHeight){
-            imgScale = startWidth/(float)imgWidth;
-        }else{
-            imgScale = startHeight/(float)imgHeight;
-        }
-        return imgScale;
-    }
-
-    private float getScale(int startWidth,int finalWidth,int startHeight,int finalHeight){
-        float imgScale;//图片实际尺寸显示在startWidth，startHeight中的缩放比例
-        float scale;//finalWidth，finalHeight要显示startWidth，finalWidth同样尺寸的图片需要的缩放比例
-        Bitmap bitmap = BitmapFactory.decodeFile(ImageloaderUtils.getImageDiscCached(mImgUrl).getAbsolutePath());
-        int imgWidth = bitmap.getWidth();
-        int imgHeight = bitmap.getHeight();
-
-        if(imgWidth >= imgHeight){
-            imgScale = startWidth/(float)imgWidth;
-        }else{
-            imgScale = startHeight/(float)imgHeight;
-        }
-
-        if(imgWidth >= imgHeight){
-            //宽的肯定没问题，直接按宽度来缩放（不用考虑宽度是否能充满，因为屏幕本来就是高的，
-            // 如果宽度无法充满那该图片就是高的）
-            scale = (float)startWidth/finalWidth;
-            Toast.makeText(this.getActivity(),"宽的",Toast.LENGTH_SHORT).show();
-        }else{
-            scale = (float)startHeight/finalHeight;
-            //如果图片比屏幕偏胖，那么高度就不能完全充满（图片高度不能充满ImageView），则要相应的放大些
-            if(imgWidth/(float)imgHeight > finalWidth/(float)finalHeight){
-                Toast.makeText(this.getActivity(),"高的-偏胖",Toast.LENGTH_SHORT).show();
-//                float percent = (finalWidth*imgHeight)/((float)finalHeight*imgWidth);//图片高度所占ImageView百分比
-//                float _height = imgScale*bitmap.getHeight()/percent;
-//                scale = (float)_height/finalHeight;
-                //高度无法充满，宽度肯定充满，所以宽度是限制，只需要把ImageView宽度缩小为图片实际显示的宽度即可
-                float _width = imgScale*imgWidth;
-                scale = _width/finalWidth;
-            }else{//反之偏瘦的话高度肯定会充满
-                Toast.makeText(this.getActivity(),"高的-偏瘦",Toast.LENGTH_SHORT).show();
-            }
-        }
-        Log.e("Test","scale===="+scale);
-        return scale;
     }
 }
--- app/src/main/java/com/msn/support/utils/ImageUtils.java
+++ app/src/main/java/com/msn/support/utils/ImageUtils.java
@@ -2,6 +2,8 @@ package com.msn.support.utils;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.msn.support.gallery.ImageloaderUtils;
 
@@ -20,5 +22,115 @@ public class ImageUtils {
         int imgWidth = bitmap.getWidth();
         int imgHeight = bitmap.getHeight();
         return new int[]{imgWidth,imgHeight};
+    }
+
+    /**
+     * 图片实际显示宽度为imgWidth；ImageView宽度为viewWidth
+     * 求padingLeft
+     * @return
+     */
+    public static float getPaddingLeft(float imgWidth,float viewWidth){
+        return (imgWidth - viewWidth)/2;
+    }
+
+    /**
+     * 图片实际显示高度为imgHeight；ImageView高度为viewHeight
+     * 求padingTop
+     *
+     * @return
+     */
+    public static float getPaddingTop(float imgHeight,float viewHeight){
+        return (imgHeight - viewHeight)/2;
+    }
+
+    /**
+     * 图片尺寸为imgWidth，imgHeight；ImageView的尺寸为startWidth，startHeight
+     * 求图片缩放模式为fitcenter时，图片的显示的实际宽度
+     * @return
+     */
+    public static float getBitmapWidth(int startWidth,int startHeight,int imgWidth, int imgHeight){
+        float actureWidth;
+
+        if(imgWidth >= imgHeight){//宽的图片，最终宽度为ImageView宽度
+            actureWidth = startWidth;
+        }else{//高的，
+            float actureHeight = startHeight;
+            float bitmapScale = actureHeight/imgHeight;
+            actureWidth = bitmapScale*imgWidth;
+        }
+
+        return actureWidth;
+    }
+
+    /**
+     * 图片尺寸为imgWidth，imgHeight；ImageView的尺寸为startWidth，startHeight
+     * 求图片缩放模式为fitcenter时，图片的显示的实际高度
+     * @return
+     */
+    public static float getBitmapHeight(int startWidth,int startHeight,int imgWidth, int imgHeight){
+        float actureHeight;
+
+        if(imgWidth <= imgHeight){//高的图片，最终高度为ImageView高度
+            actureHeight = startHeight;
+        }else{//宽的，
+            float actureWidth = startWidth;
+            float bitmapScale = actureWidth/imgWidth;
+            actureHeight = bitmapScale*imgHeight;
+        }
+
+        return actureHeight;
+    }
+
+    /**
+     * 图片实际尺寸显示在viewWidth，viewHeight中的缩放比例(scaleType=fitcenter)
+     * @return
+     */
+    public static float getBitmapScaleInImageView(int imgWidth,int imgHeight,int viewWidth,int viewHeight){
+        float imgScale;
+        if(imgWidth >= imgHeight){
+            imgScale = viewWidth/(float)imgWidth;
+        }else{
+            imgScale = viewHeight/(float)imgHeight;
+        }
+        return imgScale;
+    }
+
+    /**
+     * ImageView A尺寸为startWidth和startHeight，并且设置了尺寸为imgWidth和imgHeight的图片
+     * 求一个尺寸为finalWidth和finalHeight的ImageView B，
+     * 要缩小多少倍才能和ImageView A中的图片有一样的显示尺寸
+     * @return
+     */
+    public static float getScale(int imgWidth,int imgHeight,int startWidth,int finalWidth,int startHeight,int finalHeight){
+        float imgScale;//图片实际尺寸显示在startWidth，startHeight中的缩放比例
+        float scale;//finalWidth，finalHeight要显示startWidth，finalWidth同样尺寸的图片需要的缩放比例
+        if(imgWidth >= imgHeight){
+            imgScale = startWidth/(float)imgWidth;
+        }else{
+            imgScale = startHeight/(float)imgHeight;
+        }
+
+        if(imgWidth >= imgHeight){
+            //宽的肯定没问题，直接按宽度来缩放（不用考虑宽度是否能充满，因为屏幕本来就是高的，
+            // 如果宽度无法充满那该图片就是高的）
+            scale = (float)startWidth/finalWidth;
+            //Toast.makeText(this.getActivity(), "宽的", Toast.LENGTH_SHORT).show();
+        }else{
+            scale = (float)startHeight/finalHeight;
+            //如果图片比屏幕偏胖，那么高度就不能完全充满（图片高度不能充满ImageView），则要相应的放大些
+            if(imgWidth/(float)imgHeight > finalWidth/(float)finalHeight){
+                //Toast.makeText(this.getActivity(),"高的-偏胖",Toast.LENGTH_SHORT).show();
+//                float percent = (finalWidth*imgHeight)/((float)finalHeight*imgWidth);//图片高度所占ImageView百分比
+//                float _height = imgScale*bitmap.getHeight()/percent;
+//                scale = (float)_height/finalHeight;
+                //高度无法充满，宽度肯定充满，所以宽度是限制，只需要把ImageView宽度缩小为图片实际显示的宽度即可
+                float _width = imgScale*imgWidth;
+                scale = _width/finalWidth;
+            }else{//反之偏瘦的话高度肯定会充满
+                //Toast.makeText(this.getActivity(),"高的-偏瘦",Toast.LENGTH_SHORT).show();
+            }
+        }
+        Log.e("Test", "scale====" + scale);
+        return scale;
     }
 }
